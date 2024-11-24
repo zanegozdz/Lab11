61676e8d303982477d45a414e7fcf248bd8de5fe
Zane Gozdz <Zmgozdz@gmail.com>
2024-11-23 19:55:44 -0500

    option 1 & 2

--- Lab11.py
+++ Lab11.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import os
 
 file_path = "C:\\Users\\Zmgoz\\OneDrive\\Documents\\UF\\Fall 2024\\COP3502C\\PycharmProjects\\COP3502C\\Labs\\11-Module14LabGradeCalculator\\data\\submissions"
@@ -26,11 +26,12 @@ while True:
                     student_id = student[0:3]
                     students_info[student_name] = student_id
 
-
+        submissions_check = []
         for files in os.listdir("data\\submissions"):
             submissions_file = os.path.join(file_path, files)
             with open(submissions_file, "r") as f:
                 submissions = f.readline().split("|")
+                submissions_check.append(submissions)
                 student_code = submissions[0]
                 assignment_code = submissions[1]
                 grade_percentage = submissions[2]
@@ -44,6 +45,8 @@ while True:
                             max_points = int(lines[i+1])
                             total_points += int(lines[i+1])
                             points_earned += (int(grade_percentage) / 100) * int(max_points)
+        if student_name not in submissions_check:
+            print("Student not found")
         print(f"{round((points_earned/total_points) * 100)}%")
     elif option == 2:
         min_grade = 0
@@ -59,17 +62,43 @@ while True:
             for i in range(len(lines)):
                 if lines[i] == assignment:
                     assignment_code = lines[i+1]
+        submissions_check = []
         for files in os.listdir("data\\submissions"):
             submissions_file = os.path.join(file_path, files)
             with open(submissions_file, "r") as f:
                 submissions = f.readline().split("|")
+                submissions_check.append(submissions)
                 if submissions[1] == assignment_code:
                     percentages.append(int(submissions[2]))
+        if assignment_code not in submissions_check:
+            print("Assignment not found")
         min_grade = int(min(percentages))
         max_grade = int(max(percentages))
         average_grade = int(sum(percentages) / len(percentages))
         print(f"Min: {min_grade}%\nAvg: {average_grade}%\nMax: {max_grade}%")
     else:
+        assignment_code = ""
+        percentages = []
+        submissions_check = []
+        assignment = input("What is the assignment name: ")
+        with open("data\\assignments.txt", "r") as f:
+            lines = []
+            for line in f.readlines():
+                lines.append(line.strip())
+            for i in range(len(lines)):
+                if lines[i] == assignment:
+                    assignment_code = lines[i + 1]
+        for files in os.listdir("data\\submissions"):
+            submissions_file = os.path.join(file_path, files)
+            with open(submissions_file, "r") as f:
+                submissions = f.readline().split("|")
+                submissions_check.append(submissions)
+                if submissions[1] == assignment_code:
+                    percentages.append(int(submissions[2]))
+        plt.hist(percentages, bins=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100])
+        plt.show()
+        if assignment_code not in submissions_check:
+            print("Assignment not found")
 
 
 
